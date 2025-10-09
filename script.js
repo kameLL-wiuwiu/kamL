347c81f29047781e0fa0cf42c1ba541c25bb2601
kameLL-wiuwiu <noviaardinacamelia@gmail.com>
2025-10-09 12:39:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = documen.getElementById('sapaButton');
+cons tombolSapa = documen.getElementById('sapaButton');
 //2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click',function() {
   //3. Jalankan kode ini ketika tombol di click
